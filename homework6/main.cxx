2409b02858d693805480f7d74d2ee72b7bf4e906
Verena Hehl <ve8730he-s@student.lu.se>
2024-10-11 16:55:23 +0200

    Step 4, factoring out the while-loop

--- homework6/main.cxx
+++ homework6/main.cxx
@@ -1,6 +1,14 @@
 #include <iostream>
 #include <string>
 
+int checksum(std::string input) {
+  int sum = 0;
+  for (int i=0; i< static_cast<int>(input.size()); i++) {
+    sum += input[i];
+  }
+  return sum;
+}
+
 int main(int arg_count, char *arg_list[]) 
 {
   if (arg_count == 3) {
@@ -8,28 +16,25 @@ int main(int arg_count, char *arg_list[])
     auto char1_arg1{*(arg_list[1])}; 
     auto filename_len{filename.size()};
     auto int_number_arg2{std::atoi(arg_list[2])};
-    auto var6{0};
-    auto var7{0};
+    //auto var6{0};
+    //auto var7{0}; //this is just an index
     std::string arg1{arg_list[1]};
 
 //var2 = filename
 //var3 = stores the first character of arg1=MNXB11, thus it is M
 //var4 = length of the filename 
 //var5 = arg2=23553, the std::atoi converts 23553 which is currently stored as a string to an integer
-//var8 = is again the MNXB11, saved as string??
+//var6 = sum of the ASCII values of MNXB11 (each letter has a ASCII number corresponding to it), renamed to sum
+//var7 = index that goes through MNXB11, not needed anymore
+//var8 = is again the MNXB11, saved as string
 
+  int sum = checksum(arg_list[1]);
 
-
-    while (true) {
-      var6 += arg1[var7++];
-      if (var7 >= static_cast<int>(arg1.size())) {
-        break;
-      }
-    }
-    if ((var6 ^ char1_arg1 * 3) << (filename_len & 0x1f) == int_number_arg2) {
-      std::cout << "Correct!" << std::endl;
-    } else {
-      std::cout << "Wrong!" << std::endl;
-    }
+  if ((sum ^ char1_arg1 * 3) << (filename_len & 0x1f) == int_number_arg2) {
+    std::cout << "Correct!" << std::endl;
+  } else {
+    std::cout << "Wrong!" << std::endl;
+  }
   }
 }
+
